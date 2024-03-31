dfbfe669205d8c31171dcf13f05da7ec86293ada
Lotfi Jellali <150339814+LotfiJL@users.noreply.github.com>
2024-03-31 05:31:22 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,12 @@ const PORT = process.env.PORT;
 
 
 // Add CORS middleware
-app.use(
-  cors({
-    origin: ["https://my-portfolio-2-front.vercel.app"],
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
