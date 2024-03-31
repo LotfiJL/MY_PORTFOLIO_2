8a51336ebfc10596e1f8efb2e6d2608bfe2ab23c
Lotfi Jellali <150339814+LotfiJL@users.noreply.github.com>
2024-03-31 02:00:12 +0100

    Update ContactMe.jsx

--- client/src/Pages/Home/ContactMe.jsx
+++ client/src/Pages/Home/ContactMe.jsx
@@ -24,7 +24,7 @@ export default function ContactMe() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "my-portfolio-2-app2.vercel.app/submit-form",
+        "https://my-portfolio-2-app2.vercel.app//submit-form",
         formData
       );
       console.log(response.data);
